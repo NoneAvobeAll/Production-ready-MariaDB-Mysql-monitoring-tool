faa484fd76e9a96b17ba0a2fbc61c33144926901
Abubakkar Khan Fazla Rabbi <abubakkerkhan7@gmail.com>
2025-09-29 14:50:29 +0600

    config.sh monitor.sh notify.sh added

--- utils/notify.sh
+++ utils/notify.sh
@@ -1,2 +1,10 @@
-# Optional Slack/email alerts
-# Add your notification logic here
+#!/bin/bash
+# Author: Abubakkar
+# Send Slack notification (optional)
+
+MESSAGE="$1"
+
+if [ "$ENABLE_SLACK" = true ]; then
+    curl -s -X POST -H 'Content-type: application/json' \
+        --data "{\"text\":\"$MESSAGE\"}" "$SLACK_WEBHOOK_URL"
+fi
